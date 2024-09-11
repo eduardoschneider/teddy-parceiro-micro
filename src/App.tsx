bc8eebc3aa558affe25d9408c3845b4612297a49
Eduardo Schneider <eduardo@fidelizi.com.br>
2024-09-11 01:13:38 -0300

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const App = () => {
+  return (
+    <div>
+      <h1>Hello from Micro Frontend! Parceiros </h1>
+    </div>
+  );
+};
+
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,69 @@
+const path = require('path');
+const { ModuleFederationPlugin } = require('webpack').container;
+
+module.exports = {
+  entry: './src/index.js',
+  mode: 'development',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    publicPath: 'auto',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx|ts|tsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react', '@babel/preset-typescript'],
+          },
+        },
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.scss$/,
+        use: ['style-loader', 'css-loader', 'sass-loader'],
+      },
+      {
+        test: /\.(svg|png|jpg|gif)$/,
+        type: 'asset/resource',
+      },
+    ],
+  },
+  plugins: [
+    new ModuleFederationPlugin({
+      name: 'parceirosMicroFrontEnd',
+      filename: 'remoteEntry.js',
+      exposes: {
+        './App': './src/App',  // Expondo o App.js
+      },
+      shared: {
+        react: {
+          singleton: true,
+          eager: true,
+          requiredVersion: '^18.0.0',
+        },
+        'react-dom': {
+          singleton: true,
+          eager: true,
+          requiredVersion: '^18.0.0',
+        },
+      },
+    }),
+  ],
+  resolve: {
+    extensions: ['.js', '.jsx', '.ts', '.tsx'],
+  },
+  devServer: {
+    static: path.join(__dirname, 'dist'),
+    compress: true,
+    port: 3002,
+    hot: true,
+    historyApiFallback: true,  // Adiciona isso para suportar SPA
+  },
+};
